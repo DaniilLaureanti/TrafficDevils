3102b623187d5baebe2932d38667fc3e0a434cdb
daniillaureanti <daniil.laureanti@gmail.com>
2021-09-29 13:47:15 +0300

    First release

---
+++ app/src/main/java/com/google/android/trafficdevils/GameFragment.java
@@ -0,0 +1,21 @@
+package com.google.android.trafficdevils;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GameFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_game, container, false);
+
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/google/android/trafficdevils/WebViewFragment.java
@@ -0,0 +1,47 @@
+package com.google.android.trafficdevils;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_web_view, container, false);
+
+        WebView webView = view.findViewById(R.id.wvSite);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://html5test.com/");
+
+        WebViewClient webViewClient = new WebViewClient() {
+            @SuppressWarnings("deprecation")
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @TargetApi(Build.VERSION_CODES.N)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return true;
+            }
+        };
+
+        webView.setWebViewClient(webViewClient);
+
+        return view;
+    }
+}

ada6876b05e29c3e90979cf2197d822ae8d2d3b4
daniillaureanti <daniil.laureanti@gmail.com>
2021-09-29 14:11:14 +0300

    Add test menu

--- app/src/main/java/com/google/android/trafficdevils/MainActivity.java
+++ app/src/main/java/com/google/android/trafficdevils/MainActivity.java
@@ -1,8 +1,12 @@
 package com.google.android.trafficdevils;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,4 +17,41 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, 0, 0, "Game");
+        menu.add(0, 1, 1, "Web View");
+        menu.add(0, 2, 2, "Load Server");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == 0){
+
+            toastShow(item);
+
+        }else if (item.getItemId() == 1){
+
+            toastShow(item);
+
+        }else if (item.getItemId() == 2){
+            toastShow(item);
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void toastShow(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void toastShow(MenuItem item) {
+        toastShow(item.getTitle().toString());
+    }
+
+
+
+
 }
